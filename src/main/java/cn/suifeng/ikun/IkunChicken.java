1e3bb7174d8e038985c5c404966c43001ae347b2
suifeng7663 <suifeng7663@outlook.com>
2022-10-03 16:39:15 +0800

    修改了主类和一些模组信息

---
+++ src/main/java/cn/suifeng/ikun/IkunChicken.java
@@ -0,0 +1,17 @@
+package cn.suifeng.ikun;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(IkunChicken.MOD_ID)
+public class IkunChicken {
+    public static final String MOD_ID = "ikunchicken";
+    public static final Logger LOGGER = LogManager.getLogger();
+
+    public IkunChicken() {
+
+    }
+}
