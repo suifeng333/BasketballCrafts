73199155712c82d2e3d3a7929c9fd977952eb6ee
suifeng7663 <suifeng7663@outlook.com>
2022-10-04 19:14:22 +0800

    修改了物品注册

--- src/main/java/cn/suifeng/ikun/IkunChicken.java
+++ src/main/java/cn/suifeng/ikun/IkunChicken.java
@@ -1,25 +1,16 @@
 package cn.suifeng.ikun;
 
 import cn.suifeng.ikun.world.item.ItemList;
-import net.minecraft.world.item.Item;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 @Mod(IkunChicken.MOD_ID)
 public class IkunChicken {
     public static final String MOD_ID = "ikunchicken";
-    public static final Logger LOGGER = LogManager.getLogger();
-
-    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS,"ikunchicken");
-
 
     public IkunChicken() {
-        ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+        ItemList.ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
 
         MinecraftForge.EVENT_BUS.register(this);
     }
--- src/main/java/cn/suifeng/ikun/world/item/ItemList.java
+++ src/main/java/cn/suifeng/ikun/world/item/ItemList.java
@@ -1,10 +1,12 @@
 package cn.suifeng.ikun.world.item;
 
 import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
-import static cn.suifeng.ikun.IkunChicken.ITEMS;
-
 public class ItemList {
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS,"ikunchicken");
+
     public static final RegistryObject<Item> BASKETBALL = ITEMS.register("basketball",() -> new Item(new Item.Properties().tab(TabList.TAB_IKUNCHICKEN)));
 }
