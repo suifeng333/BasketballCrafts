445c83977dea644b564b31c7a5ddab9e9b42c8a2
suifeng7663 <suifeng7663@outlook.com>
2022-10-05 23:19:10 +0800

    新增食物荔枝

---
+++ src/main/java/cn/suifeng/ikun/world/item/FoodList.java
@@ -0,0 +1,7 @@
+package cn.suifeng.ikun.world.item;
+
+import net.minecraft.world.food.FoodProperties;
+
+public class FoodList {
+    public static final FoodProperties LITCHI = (new FoodProperties.Builder()).nutrition(6).saturationMod(0.3F).build();
+}
