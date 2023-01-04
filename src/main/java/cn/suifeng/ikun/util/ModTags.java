35dc0453373bdb466b01e27c2fc6a2c565b1d005
suifeng7663 <suifeng7663@outlook.com>
2023-01-04 17:41:49 +0800

    追加leaves和logs的tag标签，完整的一棵树

---
+++ src/main/java/cn/suifeng/ikun/util/ModTags.java
@@ -0,0 +1,14 @@
+package cn.suifeng.ikun.util;
+
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.common.Tags;
+
+public class ModTags {
+    public static class Blocks {
+
+    }
+
+    public static class Items {
+
+    }
+}
