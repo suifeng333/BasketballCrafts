67bf01b6bdc93f887e5b69ded8d82f9fa0015331
suifeng7663 <suifeng7663@outlook.com>
2023-01-06 23:49:40 +0800

    篮球模型和渲染（未测试）

---
+++ src/main/java/cn/suifeng/ikun/client/renderer/entity/BasketballRenderer.java
@@ -0,0 +1,40 @@
+package cn.suifeng.ikun.client.renderer.entity;
+
+import cn.suifeng.ikun.IkunChicken;
+import cn.suifeng.ikun.client.model.BasketballModel;
+import cn.suifeng.ikun.world.entity.BasketballEntity;
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.entity.EntityRenderer;
+import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.client.renderer.texture.OverlayTexture;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.client.gui.OverlayRegistry;
+
+public class BasketballRenderer extends EntityRenderer<BasketballEntity> {
+    private final EntityRendererProvider.Context context;
+    private static final ResourceLocation TEXTURE = new ResourceLocation(IkunChicken.MOD_ID, "textures/entity/basketball1.png");
+
+    protected BasketballRenderer(EntityRendererProvider.Context p_174008_, EntityRendererProvider.Context context) {
+        super(p_174008_);
+        this.context = context;
+    }
+
+    @Override
+    public ResourceLocation getTextureLocation(BasketballEntity pEntity) {
+        return null;
+    }
+
+    public BasketballModel getModel() {
+        return new BasketballModel<>(this.context.bakeLayer(BasketballModel.LAYER_LOCATION));
+    }
+
+    @Override
+    public void render(BasketballEntity pEntity, float pEntityYaw, float pPartialTicks, PoseStack pMatrixStack, MultiBufferSource pBuffer, int pPackedLight) {
+        super.render(pEntity, pEntityYaw, pPartialTicks, pMatrixStack, pBuffer, pPackedLight);
+        VertexConsumer cutoutNoCull = pBuffer.getBuffer(RenderType.entityCutoutNoCull(TEXTURE));
+        this.getModel().renderToBuffer(pMatrixStack,cutoutNoCull, 250, OverlayTexture.NO_OVERLAY, 1.0F, 1.0F, 1.0F, 1.0F);
+    }
+}
