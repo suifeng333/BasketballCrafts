31e42ac76913e91930ad1ae84b2b1867f9d50785
suifeng7663 <suifeng7663@outlook.com>
2023-07-08 21:19:30 +0800

    篮球长按蓄力投射

--- src/main/java/cn/suifeng/ikun/world/item/BasketballItem.java
+++ src/main/java/cn/suifeng/ikun/world/item/BasketballItem.java
@@ -5,6 +5,7 @@ import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
@@ -12,8 +13,20 @@ import net.minecraft.world.item.UseAnim;
 import net.minecraft.world.level.Level;
 
 public class BasketballItem extends Item {
-    public BasketballItem(Item.Properties item){
+    public BasketballItem(Item.Properties item) {
         super(item);
+    }
+
+    public void releaseUsing(ItemStack pStack, Level pLevel, LivingEntity pEntityLiving, int pTimeLeft) {
+        super.releaseUsing(pStack,pLevel,pEntityLiving,pTimeLeft);
+        if (pEntityLiving instanceof Player player) {
+            if (!pLevel.isClientSide) {
+                BasketballEntity basketballentity = new BasketballEntity(pLevel, player);
+                basketballentity.shootFromRotation(player, player.getXRot(), player.getYRot(), 0.0F, 1.5F, 1.0F);
+                basketballentity.setPos(player.getEyePosition());
+                pLevel.addFreshEntity(basketballentity);
+            }
+        }
     }
 
     public UseAnim getUseAnimation(ItemStack stack) {
@@ -25,6 +38,7 @@ public class BasketballItem extends Item {
     }
 
     public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand interactionHand) {
+        player.startUsingItem(interactionHand);
         ItemStack itemstack = player.getItemInHand(interactionHand);
         level.playSound(player,player.getX(),player.getY(),player.getZ(), SoundEvents.EGG_THROW, SoundSource.PLAYERS,0.5F,0.4F / (level.getRandom().nextFloat() * 0.4F + 0.8F));
         player.getEyePosition();
@@ -32,9 +46,7 @@ public class BasketballItem extends Item {
             BasketballEntity basketballentity = new BasketballEntity(level, player);
             basketballentity.shootFromRotation(player, player.getXRot(), player.getYRot(), 0.0F, 1.5F, 1.0F);
             basketballentity.setPos(player.getEyePosition());
-            level.addFreshEntity(basketballentity);
         }
-        player.startUsingItem(interactionHand);
         return InteractionResultHolder.sidedSuccess(itemstack,level.isClientSide());
     }
 }
